1ed2a6431512f0b72f09c2c06c04a6efb801dd5a
Eneas U de Queiroz <cote2004-github@yahoo.com>
2018-06-03 21:26:12 -0300

    Make it build using OpenSSL 1.1.0
    This is a rebase of Kurt Roeckx <kurt@roeckx.be> patch.
    
    Signed-off-by: Eneas U de Queiroz <cote2004-github@yahoo.com>

--- monitor.c
+++ monitor.c
@@ -590,9 +590,12 @@ mm_answer_moduli(int sock, struct sshbuf *m)
 		return (0);
 	} else {
 		/* Send first bignum */
+		const BIGNUM *p, *g;
+
+		DH_get0_pqg(dh, &p, NULL, &g);
 		if ((r = sshbuf_put_u8(m, 1)) != 0 ||
-		    (r = sshbuf_put_bignum2(m, dh->p)) != 0 ||
-		    (r = sshbuf_put_bignum2(m, dh->g)) != 0)
+		    (r = sshbuf_put_bignum2(m, p)) != 0 ||
+		    (r = sshbuf_put_bignum2(m, g)) != 0)
 			fatal("%s: buffer error: %s", __func__, ssh_err(r));
 
 		DH_free(dh);
